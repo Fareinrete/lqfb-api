f7d178e2bee649c516cce1be721dd846df6c14a3
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-12-18 02:04:04 +0100

    Add a couple of APIs

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -4,10 +4,17 @@ class UserController extends BaseController {
 
     public function showActivatedJSON()
     {
+        $actived = Member::where('active', 'TRUE')->count());
+        $unactived = Member::where('active', 'FALSE')->count());
         $a = array(array('Stato','Utenti'),
-             array('Attivi', Member::where('active', 'TRUE')->count()),
-             array('Non attivi', Member::where('active', 'FALSE')->count()));
+             array('Attivi', $actived,
+             array('Non attivi', $unactived);
         return Response::json($a);
     }
 
+    public function showActivationsJSON()
+    {
+        $out = DB::raw('SELECT date_trunc("day", activated) AS "Day" , count(*) AS "No. of users" FROM member WHERE active IS TRUE GROUP BY 1 ORDER BY 1;');
+        return Response::json($out);
+    }
 }
--- app/routes.php
+++ app/routes.php
@@ -2,4 +2,10 @@
 
 Route::get('/', 'HomeController@showHome');
 Route::get('users', 'HomeController@showUsers');
+
+// Admins
+Route::get('api/admins/activated/', 'AdminController@showActivatedJSON');
+
+// Users
 Route::get('api/users/activated/', 'UserController@showActivatedJSON');
+Route::get('api/users/activations/', 'UserController@showActivationsJSON');
